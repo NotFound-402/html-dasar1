74632a2f3c6499479117e263a11eaaa61a5d00d0
notfound404 <jeezstok2@gmail.com>
2026-04-16 14:04:02 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+const form = document.getElementById("formKirim");
+const alertBox = document.getElementById("alert");
+
+form.addEventListener("submit", function(e) {
+  e.preventDefault(); 
+
+  alertBox.classList.add("show");
+
+  setTimeout(() => {
+    alertBox.classList.remove("show");
+  }, 5000);
+}); 
